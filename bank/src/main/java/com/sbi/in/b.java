27e0a6b5f9b74e1ecd4ea5e5695e338cae447a07
yuva yuva <viswam36@gmail.com>
2017-11-01 13:57:59 +0530

    modified b

--- bank/src/main/java/com/sbi/in/b.java
+++ bank/src/main/java/com/sbi/in/b.java
@@ -4,10 +4,13 @@ package com.sbi.in;
  * Hello world!
  *
  */
+ 
 public class App 
+{
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+}
 }
